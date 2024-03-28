3da4540bae52e7f9e0c386ebdb119160b91368aa
Amawers <dacoroon.christianjericho@gmail.com>
2024-03-28 16:24:40 +0800

    fix: delete unnecessary database query

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,20 +1,14 @@
 const jwt = require('jsonwebtoken')
-const models = require('../models/index')
 
 const authenticateUser = async (req, res, next) => {
   const header = req.headers['authorization']
   const token = header.split(' ')[1]
 
   try {
-    const decoded = jwt.verify(token, 'temporary secret key');
-    const user = await models.User.findAll({
-      where: {
-        id: decoded.id,
-      }
-    })
-    if (user) next();
+    const decoded = jwt.verify(token, 'temporary secret key')
+    if (decoded) next()
   } catch (error) {
-    res.send(error);
+    res.send(error)
   }
 }
 module.exports = { authenticateUser }
